971e3cd5208057785eded0ae70d2a8a18b71c9db
Csaba Csecskedi <csaba.csecskedi@possible.com>
2018-08-16 22:46:48 +0200

    platform index magic

--- src/Game/Platform.ts
+++ src/Game/Platform.ts
@@ -13,22 +13,6 @@ namespace Game {
             ctx.fillStyle = "blue";
             ctx.fillRect(pos.x - scale.x * .45, pos.z - scale.z * .45, scale.x * .9, scale.z * .9);
         }
-
-        update(speed, from, to) {
-            let pos = this.transform.translate,
-                scale = this.transform.scale;
-            pos.z += speed;
-            if (pos.z > to) {
-                pos.z = from;
-            }
-            scale.x = 1;
-            if (pos.z < from + 1) {
-                scale.x = pos.z - from;
-            } else if (pos.z > to - 1) {
-                scale.x = to - pos.z; 
-            }
-            scale.z = scale.x;
-        }
     }
 
 }
--- src/Game/Scene.ts
+++ src/Game/Scene.ts
@@ -10,14 +10,17 @@ namespace Game {
 
     export class Scene implements SceneInterface {
 
+        row: number = 9; // active row
+        index: number = 28; // active platform
+        speed: number = .05; // move speed
         hero: Hero = new Hero();
         platforms: Platform[] = [];
 
         constructor() {
-            for (let z = -9; z <= 2; z++) {
+            for (let z = -9; z < 2; z++) {
                 for (let x = -1; x <= 1; x++) {
                     let platform = new Platform();
-                    platform.transform.translate.set(x, 0, z);
+                    platform.transform.translate.set(x, -1, z);
                     this.platforms.push(platform);
                 }
             }
@@ -39,7 +42,7 @@ namespace Game {
             ctx.save();
             ctx.translate(Math.round(ctx.canvas.width / 2), Math.round(ctx.canvas.height / 1.2));
             ctx.scale(64, 64);
-            this.platforms.forEach(platform => {
+            this.platforms.forEach((platform, i) => {
                 platform.render(ctx);
             });
             this.hero.render(ctx);
@@ -47,10 +50,26 @@ namespace Game {
         }
         
         update(): void {
-            this.platforms.forEach(platform => {
-                platform.update(.05, -9, 2);
-            });
             this.hero.update();
+            this.platforms.forEach(platform => {
+                let pos = platform.transform.translate;
+                pos.z += this.speed;
+                if (pos.z > 2) {
+                    pos.z -= 11;
+                }
+                let scale = 1;
+                if (pos.z < -8) {
+                    scale = pos.z + 9;
+                } else if (pos.z > 1) {
+                    scale = 2 - pos.z; 
+                }
+                platform.transform.scale.set(scale, scale, scale);
+            });
+            this.row -= this.speed;
+            if (this.row <= -.5) {
+                this.row += 11; 
+            }
+            this.index = Math.round(this.row) * 3 + Math.round(this.hero.transform.translate.x) + 1;
         }
 
     }
